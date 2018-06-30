6b484290c5c9cda34ced0b10167164b375880491
chibusoft <otichibueze@gmail.com>
2018-06-30 15:37:04 +0100

    update html skeleton implement currency dropdown list from api get exchange rate based off on selection add node js to host page add service worker add cache seperate exchange javascript from html implement idb indexDb to save viewed rates and get it in offline mode

---
+++ exchange.js
@@ -0,0 +1,191 @@
+
+
+	if('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		   .register('/sw.js')
+		   .then(function() {
+		   console.log("Service Worker Registered");
+		   })
+		   .catch(function(err){ console.log('Error couldnt register serviceWorker');})
+		   ;
+	}
+	
+	document.addEventListener("DOMContentLoaded",function(){
+	
+	 LoadDropList();
+	 
+		//Prevent default button click
+		document.getElementById("convertbtn").addEventListener("click", function(event){
+		event.preventDefault()
+		});
+	});
+	
+	
+	function LoadDropList() {
+
+        console.log('started my function');
+
+            const currencies = 'https://free.currencyconverterapi.com/api/v5/currencies?';
+
+			//this method is to fetch url
+			fetch(currencies, {
+			method: 'get'
+			}).then(function(response) {
+				console.log('successful');
+				return response.json();
+			}).then(function(data) {
+			
+			//get object count
+			let count = Object.keys(data.results).length;
+			
+			console.log(count);
+		
+			//here we get reference to dropdown
+			let fromOptions = document.getElementById('from_currency');
+			let toOptions = document.getElementById('to_currency');
+			
+			//here we loop through to populate dropdown
+			for (let i = 0; i < count; i++) {
+				
+				let to_values = document.createElement("option"); 
+				to_values.textContent = Object.values(data.results)[i].currencyName;
+				to_values.value = Object.values(data.results)[i].id;
+				
+				let from_values = document.createElement("option"); 
+				from_values.textContent = Object.values(data.results)[i].currencyName;
+				from_values.value = Object.values(data.results)[i].id;
+				
+				toOptions.appendChild(to_values);
+				fromOptions.appendChild(from_values);
+			}
+			})
+			.catch(function(err) {
+				console.log(`Error failure  ${err}`);
+			});
+
+    };
+	
+
+	function setRate(ex_value)
+	{
+		console.log(`rate is ${ex_value} from getRate function`);
+		
+				//here we get reference to html id's
+				let fromOptions = document.getElementById('from_currency');
+				let toOptions = document.getElementById('to_currency');
+				let text_description = document.getElementById('description');
+				const Input_Amount = document.getElementById('InputAmount').value
+				let result = document.getElementById('result');
+
+				const from_selected = {
+				value : fromOptions.options[fromOptions.selectedIndex].value,
+				Text : fromOptions.options[fromOptions.selectedIndex].textContent
+				};
+
+				const to_selected = {
+				value : toOptions.options[toOptions.selectedIndex].value,
+				Text : toOptions.options[toOptions.selectedIndex].textContent
+				};
+		
+				let value = ex_value;
+						
+				console.log(`value on exhange class is ${value}`);
+
+			if(value != null)
+				{
+					//get json returned value
+					const exchange_value = value;
+
+					//Display exchange value in text
+					text_description.innerHTML = `Exchange rate from  ${from_selected.Text} to ${to_selected.Text} is ${exchange_value}`;
+
+
+					//Display conversion
+					//console.log(`Input value = ${InputAmount}`);
+					let my_result = Input_Amount * exchange_value;
+
+					console.log(`result value from database = ${my_result}`);
+					result.innerHTML	 = `${to_selected.value} ${my_result}`;
+				}
+				else{
+					console.log('you are offline cannot get values');
+				}
+	};
+	
+	function GetExchange() {
+	
+			//here we get reference to html id's
+			let fromOptions = document.getElementById('from_currency');
+			let toOptions = document.getElementById('to_currency');
+			let text_description = document.getElementById('description');
+			const Input_Amount = document.getElementById('InputAmount').value
+			let result = document.getElementById('result');
+			
+			const from_selected = {
+			value : fromOptions.options[fromOptions.selectedIndex].value,
+			Text : fromOptions.options[fromOptions.selectedIndex].textContent
+			};
+			
+			const to_selected = {
+			value : toOptions.options[toOptions.selectedIndex].value,
+			Text : toOptions.options[toOptions.selectedIndex].textContent
+			};
+			
+			
+			
+			console.log(`From selected = ${from_selected.value}`);
+			console.log(`To selected currency = ${to_selected.value}`);
+			
+			
+			
+				if(navigator.onLine){
+				
+						const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${from_selected.value}_${to_selected.value}&compact=ultra`;
+
+						console.log(`selected url api = ${url}`);
+
+
+						//fetch rate
+						fetch(url, {
+						method: 'get'
+						}).then(function(response) {
+						return response.json();
+						}).then(function(data) {
+
+						//get json returned value
+						const exchange_value = Object.values(data)[0];
+
+						//Display exchange value in text
+						text_description.innerHTML = `Exchange rate from  ${from_selected.Text} to ${to_selected.Text} is ${exchange_value}`;
+
+
+						//Display conversion
+						//console.log(`Input value = ${InputAmount}`);
+						let my_result = Input_Amount * exchange_value;
+
+						console.log(`result value = ${my_result}`);
+						result.innerHTML	 = `${to_selected.value} ${my_result}`;
+						//result.style.display = 'block';
+
+
+						//save values
+						saveExchange(`${from_selected.value}_${to_selected.value}`,`${exchange_value}`);
+
+						})
+						.catch(function(err) {
+						console.log(`Error failure  ${err}`);
+
+						});
+
+
+
+				} else {
+						getExchange(`${from_selected.value}_${to_selected.value}`);
+				}
+		
+			};
+           
+
+		   
+		   
+
---
+++ sw.js
@@ -0,0 +1,50 @@
+
+	let staticCache = 'currency-static-v3';
+	
+	self.addEventListener('install', function(event) {
+		event.waitUntil(
+		caches.open(staticCache).then(function(cache) {
+		  return cache.addAll([
+			'/',
+			'/index.html',
+			'/mStyle.css',
+			'/exchange.js',
+			'/mdatabase.js',
+			'https://free.currencyconverterapi.com/api/v5/currencies?'
+		  ]);
+		})
+		);
+		});
+		
+		
+		self.addEventListener('activate', function(event) {
+		event.waitUntil(
+		caches.keys().then(function(cacheNames) {
+		  return Promise.all(
+			cacheNames.filter(function(cacheName) {
+			  return cacheName.startsWith('currency-') &&
+					 cacheName != staticCache;
+			}).map(function(cacheName) {
+			  return caches.delete(cacheName);
+			})
+		  );
+		})
+		);
+		});
+		
+		
+		self.addEventListener('fetch', function(event) {
+		
+		event.respondWith(
+		caches.match(event.request).then(function(response) {
+			//console.log(event.request);
+		  return response || fetch(event.request);
+		})
+		);
+		});
+
+		self.addEventListener('message', function(event) {
+		if (event.data.action === 'skipWaiting') {
+		self.skipWaiting();
+		}
+		});
